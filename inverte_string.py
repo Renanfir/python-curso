5a85c57a1df2760ca2c4a7fba65b59bb4eabbdac
Renanfir <renan260307@gmail.com>
2023-11-29 09:39:03 -0300

    novo

---
+++ inverte_string.py
@@ -0,0 +1,21 @@
+def criar_funcao(funcao):
+    def interna(*args,**kwargs):
+        
+        for arg in args:
+            is_string(arg)
+        resultado = funcao(*args,**kwargs)
+        
+        return resultado
+    return interna
+
+
+def inverte_string(string):
+    return string[::-1]
+
+def is_string(param):
+    if not isinstance(param, str):
+        raise(TypeError("Param deve ser uma string"))
+
+inverte_string_checando_parametro = criar_funcao(inverte_string)
+invertida = inverte_string_checando_parametro('123')
+print(invertida)
