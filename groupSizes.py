7be0bfc700e31c273329d00aafb9d22e7a97136e
Renanfir <renan260307@gmail.com>
2025-02-17 08:33:18 -0300

    tentativa LeetCode

---
+++ groupSizes.py
@@ -0,0 +1,24 @@
+# groupSizes = [3,3,3,3,3,1,3]
+# retorno = []
+# dicionario = {}
+# for i in range(500):
+#     if groupSizes.count(i) == 0:
+#         ...
+#     else:
+#         if i not in dicionario:
+#             dicionario[i] = []
+#         dicionario[i].append(groupSizes.count(i))
+
+# for chave,valor in dicionario.items():
+#     retorno.append(groupSizes.index(chave))
+#     # print(chave,valor)
+
+
+
+
+# print(retorno)
+
+#     # array = []
+#     # array = [valor] * chave
+#     # retorno.append(array)
+#N SEI
